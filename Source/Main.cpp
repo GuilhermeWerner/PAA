b4732c468ac25c0b8c61f52687057e669c759383
Guilherme Werner <guilherme.werner@tribufu.com>
2023-06-20 15:51:35 -0300

    Update Main.cpp

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -2,8 +2,9 @@
 
 int main()
 {
-    int nodes = 6;
-    Graph *g1 = new Graph(nodes);
+    cout << "-- GRAPH 1 --" << endl;
+    int n1 = 6;
+    Graph *g1 = new Graph(n1);
 
     g1->InsertD(0, 1);
     g1->InsertD(0, 2);
@@ -24,6 +25,28 @@ int main()
 
     cout << "\n-- PERMUTATION --" << endl;
     g1->TransitiveReduction3();
+
+    cout << "-- GRAPH 2 --" << endl;
+    int n2 = 7;
+    Graph *g2 = new Graph(n2);
+
+    g2->InsertD(0, 1);
+    g2->InsertD(0, 4);
+    g2->InsertD(1, 2);
+    g2->InsertD(1, 3);
+    g2->InsertD(1, 6);
+    g2->InsertD(3, 2);
+    g2->InsertD(4, 1);
+    g2->InsertD(5, 1);
+
+    cout << "-- MATRIX --" << endl;
+    g2->TransitiveReduction1();
+
+    cout << "\n-- LOOPS --" << endl;
+    g2->TransitiveReduction2();
+
+    cout << "\n-- PERMUTATION --" << endl;
+    g2->TransitiveReduction3();
 
     return 0;
 }
